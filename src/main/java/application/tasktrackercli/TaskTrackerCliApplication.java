8250fa2997b748465f165868e0ec703c1eae530c
João Marcos <joaomarcos.c.dev@gmail.com>
2026-07-20 21:32:04 -0300

    Created base for the project

---
+++ src/main/java/application/tasktrackercli/TaskTrackerCliApplication.java
@@ -0,0 +1,13 @@
+package application.tasktrackercli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerCliApplication.class, args);
+    }
+
+}
