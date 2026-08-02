1d9d749923accb325a541084b1c6841d4b4549a1
João Marcos <joaomarcos.c.dev@gmail.com>
2026-08-02 18:20:09 -0300

    feature:created menu interface CLI for user

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,8 @@ import cli.TaskCli;
 public class Main {
     public static void main(String[] args) {
         TaskManager manager = new TaskManager();
-
-        manager.adicionarTarefa("Estudar Java", "Revisar coleções");
-        manager.adicionarTarefa("Estudar C++", "Revisar POO");
-
         TaskCli cli = new TaskCli(manager);
-
-        cli.mostrarTarefas();
-
-
-        manager.buscarTarefaPorId(2);
-
-        manager.alterarStatusTarefa(1, Status.IN_PROGRESS);
-
-        manager.listarPorStatus(Status.IN_PROGRESS);
-
-        cli.mostrarTarefas();
+        cli.menu();
     }
 
 }
--- src/cli/TaskCli.java
+++ src/cli/TaskCli.java
@@ -1,7 +1,12 @@
 package cli;
 
+import model.Status;
 import model.Task;
 import model.TaskManager;
+
+import java.util.Scanner;
+
+
 import java.util.*;
 
 public class TaskCli {
@@ -17,4 +22,82 @@ public class TaskCli {
             System.out.println(f);
         }
     }
+
+    public void menu() {
+        Scanner sc = new Scanner(System.in);
+        boolean rodando = true;
+
+        while (true) {
+            System.out.println("|------------------------------|");
+            System.out.println("|          Welcom to           |");
+            System.out.println("|       Task Tracker CLI       |");
+            System.out.println("|------------------------------|");
+            System.out.println("|                              |");
+            System.out.println("|------------------------------|");
+            System.out.println("| Manual:                      |");
+            System.out.println("| 1 - Para criar tarefa        |");
+            System.out.println("| 2 - Para remover uma tarefa  |");
+            System.out.println("| 3 - Lista tarefas            |");
+            System.out.println("| 4 - Buscar tarefa por id     |");
+            System.out.println("| 5 - Alterar status da tarefa |");
+            System.out.println("| 6 - Alterar tarefa           |");
+            System.out.println("| 7 - Listar por Status        |");
+            System.out.println("| 8 - Encerra o programa       |");
+            System.out.println("|------------------------------|");
+            System.out.println();
+            int opcao = sc.nextInt();
+            sc.nextLine();
+            switch (opcao) {
+                case 1: {
+                    System.out.println("Para criar tarefa você precisa definir:");
+                    System.out.println("1 - Nome e 2 - Descrição");
+                    String taskName = sc.nextLine();
+                    String taskDescription = sc.nextLine();
+                    manager.adicionarTarefa(taskName, taskDescription);
+                    break;
+                }
+                case 2: {
+                    System.out.println("Para remover tarefa você precisa: ");
+                    System.out.println("1 - Id da tarefa");
+                    int taskId = sc.nextInt();
+                    manager.removerTarefa(taskId);
+                    break;
+                }
+                case 3:
+                    mostrarTarefas();
+                    break;
+                case 4: {
+                    int taskId = sc.nextInt();
+                    manager.buscarTarefaPorId(taskId);
+                    break;
+                }
+                case 5: {
+                    int taskId = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Digite o status (TODO, IN_PROGRESS, DONE):");
+                    String textoStatus = sc.nextLine();
+                    Status status = Status.valueOf(textoStatus.toUpperCase());
+                    manager.alterarStatusTarefa(taskId, status);
+                    break;
+                }
+                case 6: {
+                    int taskId = sc.nextInt();
+                    String taskDescription = sc.nextLine();
+                    manager.alterarTarefa(taskId, taskDescription);
+                    break;
+
+                }
+                case 7:
+                    manager.listarTarefa();
+                    break;
+                case 8:
+                    rodando = false;
+                    break;
+                default:
+                    System.out.println("Opção Invalida");
+            }
+        }
+
+    }
+
 }
